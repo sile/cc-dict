2b8cef5768e8d22ae5efc09b5bd7c88527eeb461
Takeru Ohta <phjgt308@gmail.com>
2011-12-11 23:24:28 +0900

    アロケータにsol用のメソッド追加

--- include/dict/allocator.hh
+++ include/dict/allocator.hh
@@ -64,123 +64,34 @@ namespace dict {
       return p;
     }
 
-    void release(void* ptr) {
-      chunk* free = reinterpret_cast<chunk*>(ptr);
-      chunk* head = peek_chunk();
-      free->free_next = head->free_next;
-      head->free_next = free;
-      
-      recycle_count++;
-    }
-
-    void clear() {
-      delete block->prev;
-      block->prev = NULL;
-      position = 0;
-      recycle_count = 0;
-    }
-
-  private:
-    chunk* read_chunk() {
-      chunk* p = peek_chunk();
-      if(++position == block->size)
-        enlarge();      
-      return p;
-    }
-    
-    chunk* peek_chunk() const {
-      return block->chunks + position;
-    }
-    
-    void enlarge () {
-      chunk_block* new_block = new chunk_block(block->size*1.25);
-      new_block->prev = block;
-      block = new_block;
-      position = 0;
-    }
-
-  private:
-    chunk_block* block;
-    unsigned position;
-    unsigned recycle_count;
-  };
-
-  template<unsigned CHUNK_SIZE>
-  class fixed_size_allocator2 { 
-  private:
-    static const unsigned INITIAL_BLOCK_SIZE=8;
-
-    struct chunk {
-      union {
-        char buf[CHUNK_SIZE];
-        chunk* free_next;
-      };
-    };
-    
-    struct chunk_block {
-      chunk* chunks;
-      unsigned size;
-      chunk_block* prev;
-      
-      chunk_block(unsigned size) : chunks(NULL), size(size), prev(NULL) {
-        chunks = new chunk[size];
-      }
-      
-      ~chunk_block() {
-        delete [] chunks;
-        delete prev;
-      }
-    };
-
-  public:
-    fixed_size_allocator2()
-      : block(NULL), position(0), recycle_count(0) {
-      block = new chunk_block(INITIAL_BLOCK_SIZE);
-    }
-      
-    ~fixed_size_allocator2() {
-      clear();
-      delete block;
-    }
-    
-    void* allocate() {
-      chunk* p;
-      
-      if(recycle_count > 0) {
-        chunk* head = peek_chunk();
-        p = head->free_next;
-        head->free_next = p->free_next;
-
-        recycle_count--;
-      } else {
-        p = read_chunk();
-      }
-      
-      return p;
-    }
-
-    void release(void* ptr) {
-      chunk* free = reinterpret_cast<chunk*>(ptr);
-      chunk* head = peek_chunk();
-      free->free_next = head->free_next;
-      head->free_next = free;
-      
-      recycle_count++;
-    }
-
-    void clear() {
-      delete block->prev;
-      block->prev = NULL;
-      position = 0;
-      recycle_count = 0;
-    }
-
-    void* alloc_block(unsigned count) {
-      chunk_block* new_block = new chunk_block(count+1);
+    void* allocate_block(unsigned count) {
+      chunk_block* new_block = new chunk_block(count*2);
       new_block->prev = block;
       block = new_block;
       position = count;
+      recycle_count = 0;
       return block->chunks;
+    }
+
+    void release_old_blocks() {
+      delete block->prev;
+      block->prev = NULL;
+    }   
+
+    void release(void* ptr) {
+      chunk* free = reinterpret_cast<chunk*>(ptr);
+      chunk* head = peek_chunk();
+      free->free_next = head->free_next;
+      head->free_next = free;
+      
+      recycle_count++;
+    }
+
+    void clear() {
+      delete block->prev;
+      block->prev = NULL;
+      position = 0;
+      recycle_count = 0;
     }
 
   private:
@@ -202,7 +113,7 @@ namespace dict {
       position = 0;
     }
 
-  private:
+  protected:
     chunk_block* block;
     unsigned position;
     unsigned recycle_count;
--- include/dict/sol_map.hh
+++ include/dict/sol_map.hh
@@ -11,10 +11,10 @@
 #include "eql.hh"
 #include "allocator.hh"
 #include <cstddef>
+#include <cstdlib>
 
 namespace dict {
   namespace {
-    // XXX: assume as sizeof(unsigned) is 4
     inline unsigned bit_reverse(unsigned n) {
       n = ((n&0x55555555) << 1) | ((n&0xAAAAAAAA) >> 1);
       n = ((n&0x33333333) << 2) | ((n&0xCCCCCCCC) >> 2);
@@ -23,7 +23,7 @@ namespace dict {
         ((n & 0x000000FF) << 24) |
         ((n & 0x0000FF00) <<  8) |
         ((n & 0x00FF0000) >>  8) |
-        ((n & 0xFF000000) >> 24);   // n >> 24
+        ((n & 0xFF000000) >> 24); 
     }
   }
 
@@ -46,8 +46,8 @@ namespace dict {
     };
     
   private:
-    static const unsigned MAX_HASHCODE = static_cast<unsigned>(-1);
-    static const unsigned ORDINAL_NODE_HASHCODE_MASK = MAX_HASHCODE<<1;
+    static const unsigned MAX_HASHCODE = 0xFFFFFFFF;
+    static const unsigned ORDINAL_NODE_HASHCODE_MASK = (MAX_HASHCODE<<1) & MAX_HASHCODE;
     static const unsigned INITIAL_TABLE_SIZE = 8;
 
   public:
@@ -55,14 +55,16 @@ namespace dict {
       table(NULL),
       table_size(INITIAL_TABLE_SIZE),
       rehash_border(0),
-      rehash_threshold(1.00), //0.75),
+      rehash_threshold(0.90),
       element_count(0)
     {
-      init();
+      table = reinterpret_cast<node**>(std::malloc(sizeof(node*)*table_size));
+      std::fill(table, table+table_size, const_cast<node*>(&node::tail));
+      rehash_border = table_size * rehash_threshold;
     }
 
     ~sol_map() {
-      delete [] table;
+      std::free(table);
     }
 
     Value* find(const Key& key) const {
@@ -78,74 +80,58 @@ namespace dict {
         return (*place)->value;
       
       *place = new (node_alloca.allocate()) node(key,*place,hashcode);
-      Value& value = (*place)->value;
-      if(++element_count >= rehash_border)
+      if(++element_count >= rehash_border) {
         enlarge();
-      
-      return value;
+        return operator[](key);
+      }
+
+      return (*place)->value;
     }
     unsigned size() const { return element_count; }
 
   private:
-    void init() {
-      table = new node*[table_size]; // TODO: recalloc(?) を使ってみる？
-      std::fill(table, table+table_size, const_cast<node*>(&node::tail));
-      rehash_border = table_size * rehash_threshold;
-    }
-
     void enlarge() {
-      std::cout << "IN: " << table_size << " #" << element_count << std::endl;
-
-      const unsigned old_table_size = table_size;
-      node** old_table = table;
-
+      const unsigned new_table_size = table_size<<1;
       
-      //init();
-      // XXX: new_table_size
-      table = new node*[table_size<<1]; // TODO: recalloc(?) を使ってみる？
-      rehash_border = (table_size<<1) * rehash_threshold;
-
-      /*
-      for(unsigned i=0; i < old_table_size; i++) {
-        table[i] = old_table[i];
-      }
-      */
+      table = reinterpret_cast<node**>(std::realloc(table, sizeof(node*)*new_table_size));
       
-      node* buf = (node*)node_alloca.alloc_block(element_count);
-      for(unsigned i=0; i < old_table_size; i++) {
-        //        std::cout << " - " << i << std::endl;
-        table[i] = buf;
-        if(old_table[i]==&node::tail)
-          table[i] = const_cast<node*>(&node::tail);
+      // XXX:
+      node* buf = (node*)node_alloca.allocate_block(element_count); // XXX:
+      
+      for(unsigned i=0; i < table_size; i++) {
+        if(table[i]==&node::tail)
+          continue;
         
-        for(node* c=old_table[i]; c != &node::tail; c = c->next) {
-          *buf = *c;
+        node* cur = table[i];
+        table[i] = buf;
+        for(; cur != &node::tail; cur = cur->next, buf++) {
+          *buf = *cur;
           if(buf->next != &node::tail)
             buf->next = buf+1;
-          buf++;
         }
       }
-      
-      for(unsigned i=0; i < old_table_size; i++) {
-        rehash_node(table[i], i, old_table_size);
-      }
-      
-      table_size <<= 1;
+      node_alloca.release_old_blocks(); 
 
-      delete [] old_table;
+      for(unsigned i=0; i < table_size; i++)
+        rehash_node(table[i], i);
+
+      table_size = new_table_size;
+      rehash_border = table_size * rehash_threshold;
     }
     
-    void rehash_node(node* head, unsigned old_index, unsigned old_table_size) {
-      // TODO: 整理
-      unsigned child = old_index | old_table_size; // TODO: comment (新しい最上位ビットを1にする)
-      unsigned hashcode = bit_reverse(child);
+    void rehash_node(node* head, unsigned parent_index) {
+      const unsigned child = parent_index | table_size; // 有効な最上位ビットを1にする => 子のインデックス
+      const unsigned hashcode = bit_reverse(child);
       node** place;
-      //std::cout << " - " << child << std::endl;
 
       find_candidate(hashcode, place);
-
+      
+      // table[parent] -> node1 -> node2 -> node3 -> tail
+      //  ↓
+      // table[parent] -> node1 -> tail
+      // table[child]  -> node2 -> node3 -> tail
       table[child] = *place;
-      *place = const_cast<node*>(&node::tail);
+      *place = const_cast<node*>(&node::tail);      
     }
 
     bool find_node(const Key& key, node**& place) const {
@@ -154,7 +140,7 @@ namespace dict {
     }
     
     bool find_node(const Key& key, node**& place, unsigned& hashcode) const {
-      hashcode = hash(key) << 4; // XXX: & ORDINAL_NODE_HASHCODE_MASK;
+      hashcode = bit_reverse(hash(key)) & ORDINAL_NODE_HASHCODE_MASK;
       find_candidate(hashcode, place);
 
       for(node* node=*place; hashcode==node->hashcode; node=*(place=&node->next))
@@ -165,19 +151,17 @@ namespace dict {
 
     void find_candidate(const unsigned hashcode, node**& place) const {
       const unsigned index = bit_reverse(hashcode) & (table_size-1);
-      //      std::cout << " i: " << index <<", "<< bit_reverse(hashcode) << ", " << (table_size-1) << std::endl;
       for(node* node=*(place=&table[index]); node->hashcode < hashcode; node=*(place=&node->next));
     }
 
   private:
-    fixed_size_allocator2<sizeof(node)> node_alloca;
+    fixed_size_allocator<sizeof(node)> node_alloca;
     node** table;
     unsigned table_size;
     unsigned rehash_border;
     const float rehash_threshold;
     unsigned element_count;
   };
-
 
   template<class Key, class Value, class Hash, class Eql>
   const typename sol_map<Key,Value,Hash,Eql>::node sol_map<Key,Value,Hash,Eql>::node::tail;
